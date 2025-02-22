16b774ba4842693b67c6cb919da78c1dd748b37b
eremanikita <eremanikita@gmail.com>
2025-02-23 01:45:58 +0300

    Add second cow

--- 02_PushPip/twocows.py
+++ 02_PushPip/twocows.py
@@ -2,17 +2,28 @@ from cowsay import cowsay
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-e", "--eyes", default="oo")
-parser.add_argument("-f", "--cow", default="default")
-parser.add_argument("-n", "--wrapper", action="store_true")
+parser.add_argument("-e", "--first_eyes", default="oo")
+parser.add_argument("-f", "--first_cow", default="default")
+parser.add_argument("-n", "--first_wrapper", action="store_true")
 parser.add_argument("-T", "--tongue", default='  ')
 parser.add_argument("-W", "--width", type=int, default=40)
-parser.add_argument("message", default=None, nargs='?')
+parser.add_argument("first_message", default=None, nargs='?')
+
+parser.add_argument("-E", "--second_eyes", default="oo")
+parser.add_argument("-F", "--second_cow", default="default")
+parser.add_argument("-N", "--second_wrapper", action="store_true")
+parser.add_argument("second_message", default=None, nargs='?')
 
 args = parser.parse_args()
-print(cowsay(message=args.message,
-             eyes=args.eyes,
+print(cowsay(message=args.first_message,
+             eyes=args.first_eyes,
              width=args.width,
-             cow=args.cow,
-             wrap_text=args.wrapper,
+             cow=args.first_cow,
+             wrap_text=args.first_wrapper,
+             tongue=args.tongue))
+print(cowsay(message=args.second_message,
+             eyes=args.second_eyes,
+             width=args.width,
+             cow=args.second_cow,
+             wrap_text=args.second_wrapper,
              tongue=args.tongue))
