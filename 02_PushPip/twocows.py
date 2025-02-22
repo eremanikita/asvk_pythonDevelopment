eb1373f48e3c0171d2468a61ae556f6b807deb55
eremanikita <eremanikita@gmail.com>
2025-02-23 01:57:19 +0300

    Display cows in a sequential order

--- 02_PushPip/twocows.py
+++ 02_PushPip/twocows.py
@@ -15,15 +15,26 @@ parser.add_argument("-N", "--second_wrapper", action="store_true")
 parser.add_argument("second_message", default=None, nargs='?')
 
 args = parser.parse_args()
-print(cowsay(message=args.first_message,
-             eyes=args.first_eyes,
-             width=args.width,
-             cow=args.first_cow,
-             wrap_text=args.first_wrapper,
-             tongue=args.tongue))
-print(cowsay(message=args.second_message,
-             eyes=args.second_eyes,
-             width=args.width,
-             cow=args.second_cow,
-             wrap_text=args.second_wrapper,
-             tongue=args.tongue))
+first_cow = cowsay(message=args.first_message,
+                   eyes=args.first_eyes,
+                   width=args.width,
+                   cow=args.first_cow,
+                   wrap_text=args.first_wrapper,
+                   tongue=args.tongue).split("\n")
+max_length = max(map(len, first_cow))
+second_cow = cowsay(message=args.second_message,
+                    eyes=args.second_eyes,
+                    width=args.width,
+                    cow=args.second_cow,
+                    wrap_text=args.second_wrapper,
+                    tongue=args.tongue).split("\n")
+first_rows = len(first_cow)
+second_rows = len(second_cow)
+result = []
+for i in range(max(len(first_cow), len(second_cow))):
+    print(i, first_rows, second_rows)
+    part_one = first_cow[first_rows - i - 1] if i < first_rows else ""
+    part_two = second_cow[second_rows - i - 1] if i < second_rows else ""
+    result.append(part_one + " " * (max_length - len(part_one)) + part_two + "\n")
+print(*result[::-1])
+
