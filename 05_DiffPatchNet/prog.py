8d5f983e4635e76e8d3e75659f11cea384d862a4
eremanikita <eremanikita@gmail.com>
2025-03-15 00:09:49 +0300

    Create chat

---
+++ 05_DiffPatchNet/prog.py
@@ -0,0 +1,61 @@
+import asyncio
+import cowsay
+import shlex
+
+clients = {}
+clients_names = {}
+
+
+async def chat(reader, writer):
+    me = "{}:{}".format(*writer.get_extra_info('peername'))
+    print(me)
+
+    username = None
+    clients[me] = asyncio.Queue()
+
+    send = asyncio.create_task(reader.readline())
+    receive = asyncio.create_task(clients[me].get())
+    while True:
+        done, pending = await asyncio.wait([send, receive], return_when=asyncio.FIRST_COMPLETED)
+        for q in done:
+            if q is send:
+                send = asyncio.create_task(reader.readline())
+                text = q.result().decode()
+
+                match shlex.split(text):
+                    case ["who"] if username:
+                        await clients[me].put(" ".join(clients_names.keys()))
+                    case ["cows"] if username:
+                        await clients[me].put(cowsay.list_cows() - clients.keys())
+                    case ["login", name] if not username:
+                        clients_names[name] = me
+                        username = name
+                    case ["say", name, message] if username:
+                        await clients[clients_names[name]].put(f"{username}> {message}")
+                    case ["yield", message] if username:
+                        for out in map(lambda x: clients[clients_names[x]], clients_names.keys()):
+                            if out is not clients[me]:
+                                await out.put(f"{username}> {message}")
+                    case ["quit"]:
+                        clients.pop(me)
+                        clients_names.pop(username)
+                        send.cancel()
+                        receive.cancel()
+                        print(me, "DONE")
+                        del clients[me]
+                        writer.close()
+                        await writer.wait_closed()
+                        return
+            elif q is receive:
+                receive = asyncio.create_task(clients[me].get())
+                writer.write(f"{q.result()}\n".encode())
+                await writer.drain()
+
+
+async def main():
+    server = await asyncio.start_server(chat, '0.0.0.0', 1337)
+    async with server:
+        await server.serve_forever()
+
+
+asyncio.run(main())
